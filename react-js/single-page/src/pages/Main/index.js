95a5a1b7e0a608a7fa53afbb947ca5c15a340444
EloisaPaz <eloisapaz28@gmail.com>
2019-12-16 23:15:14 -0300

    Adicionando css na pagina main

--- react-js/single-page/src/pages/Main/index.js
+++ react-js/single-page/src/pages/Main/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 export default function Main() {
   return (
-    <Title>
-      Main
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt/>
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+        <SubmitButton disable>
+          <FaPlus color="#FFF" size={14}/>
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
