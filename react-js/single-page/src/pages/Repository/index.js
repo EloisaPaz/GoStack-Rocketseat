b2c707e76bb849e1a9f111caca22f3c108241399
EloisaPaz <eloisapaz28@gmail.com>
2019-12-18 23:10:08 -0300

    Exibindo repositorio e adicionando css

--- react-js/single-page/src/pages/Repository/index.js
+++ react-js/single-page/src/pages/Repository/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import api from '../../services/api';
-// import { Container } from './styles';
+import { Loading, Owner } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
 
@@ -44,6 +47,19 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
-    return <h1>Repository</h1>;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return(
+      <Container>
+        <Owner>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+          <Link to="/">Voltar</Link>
+        </Owner>
+      </Container>
+    );
   }
 }
---
+++ react-js/single-page/src/pages/Repository/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #FFF;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  height: 10vh;
+`;
+export const Owner = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+    margin-top: 10px;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+    color: #666;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
