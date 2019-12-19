6381b66e747f85c297667eabf60a763e8766a1ed
EloisaPaz <eloisapaz28@gmail.com>
2019-12-19 00:08:27 -0300

    Listando issues

--- react-js/single-page/src/pages/Repository/styles.js
+++ react-js/single-page/src/pages/Repository/styles.js
@@ -8,6 +8,7 @@ export const Loading = styled.div`
   justify-content: center;
   height: 10vh;
 `;
+
 export const Owner = styled.div `
   display: flex;
   flex-direction: column;
@@ -38,5 +39,65 @@ export const Owner = styled.div `
     line-height: 1.4;
     text-align: center;
     max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul `
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: 	#363636;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #eee;
+          color: #696969;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
   }
 `;
